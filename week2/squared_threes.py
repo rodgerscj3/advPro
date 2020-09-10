86aff6cd59216cf2927da96870354a20a260b398
rodgerscj3 <70657971+rodgerscj3@users.noreply.github.com>
2020-09-09 20:30:42 -0400

    initial commit

---
+++ week2/squared_threes.py
@@ -0,0 +1,17 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def squared_threes():
+    return_value = []
+    # my code
+    # shouldnt go beyond
+    # here
+    return return_value
+
+if __name__ == "__main__":
+    for x in squared_threes():
+        print(x)
+
+# should print out all numbers between 0 and 99
+# that are evenly divisible by 3, then squared
+#e.g. 9, 36, 81, ...etc
