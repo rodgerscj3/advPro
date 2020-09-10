e8e1a4171dab35ed601f1443f2bbe03f9cd6ea7a
rodgerscj3 <70657971+rodgerscj3@users.noreply.github.com>
2020-09-09 21:28:04 -0400

    week3

--- week2/squared_threes.py
+++ week2/squared_threes.py
@@ -2,24 +2,10 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 def squared_threes():
-    return_value = []
-<<<<<<< HEAD
-    # my code
-    # shouldnt go beyond
-    # here
-    return return_value
-
-if __name__ == "__main__":
-    for x in squared_threes():
-        print(x)
-
-# should print out all numbers between 0 and 99
-# that are evenly divisible by 3, then squared
-#e.g. 9, 36, 81, ...etc
-=======
-    # YOUR CODE GOES HERE
-
-    # END SHOULDNT GO BEYOND HERE
+    return_value = [x / 3 for x in range(100)]
+    
+    
+ 
     return return_value
 
 if __name__ == "__main__":
@@ -33,4 +19,3 @@ if __name__ == "__main__":
    # 36
    # 81 
    # .....   etc
->>>>>>> c916b11455eb54277969b54fd95d58761540799a
