480cc121a6322a42d10a8c2a028cb151ba4dd2e7
rodgerscj3 <70657971+rodgerscj3@users.noreply.github.com>
2020-09-23 17:01:48 -0400

    week5

--- week5/adding_numbers.py
+++ week5/adding_numbers.py
@@ -3,7 +3,12 @@ import sys
 def add_them_all(filename):
     sum = 0
     ### Your code here
-
+    f = open("a.txt", "r")
+    contents = a.readlines()
+    for line in content:
+        for j in line:
+            if j.isdigit() == True:
+                sum += int(j)
     ### End of your code
     return sum
 
