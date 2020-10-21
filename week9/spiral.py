7228b6086fd1e5903f4ec84c21d7e84369e81267
rodgerscj3 <70657971+rodgerscj3@users.noreply.github.com>
2020-10-21 19:06:15 -0400

    week9

--- week9/spiral.py
+++ week9/spiral.py
@@ -1,15 +1,20 @@
 import sys
 
-size = 5
-
+size = 501
 def print_square(square):
-    print("-"*(2*size))
+    print("-"*(4*size))
     for y in range(len(square)):
         for x in range(len(square[y])):
             print("{:6}".format(square[x][y]), end="")
         print()
-    print("-"*(2*size))
-
+    print("-"*(4*size))
 square = [[f"{x}.{y}" for x in range(size)] for y in range(size)]
-
-print_square(square)
+###print_square(square)
+def equation(j):
+    i = (j-1)/2
+    n = (3+2*i*(8*i*i+15*i+13))/3
+    return n
+def answer():
+    xrange = equation(501)
+    print(xrange)
+answer()
